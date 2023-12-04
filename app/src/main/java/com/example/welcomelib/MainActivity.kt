79d3d81a3fa918fbf762d1358aecd4b4be4a7cf4
ismayil.islamzade <ismayil.islamzade@azintelecom.az>
2023-12-04 10:51:55 +0400

    new version change jdk

--- app/src/main/java/com/example/welcomelib/MainActivity.kt
+++ app/src/main/java/com/example/welcomelib/MainActivity.kt
@@ -2,12 +2,11 @@ package com.example.welcomelib
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.welcomealphalib.Welcome
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        Welcome.sayHi()
+      //  Welcome.sayHi()
     }
 }
