19a376dfad357b876c058766063ab2f56bce0b14
ismayil.islamzade <ismayil.islamzade@azintelecom.az>
2023-12-05 09:53:38 +0400

    for obscuring project ver3.2.2

--- WelcomeAlphaLib/src/main/java/com/example/welcomealphalib/Welcome.kt
+++ WelcomeAlphaLib/src/main/java/com/example/welcomealphalib/Welcome.kt
@@ -2,10 +2,17 @@ package com.example.welcomealphalib
 
 import android.util.Log
 
-public class Welcome private constructor(){
+public class Welcome constructor(){
     companion object{
         public fun sayHi(){
             Log.e("WELCOME","Hi everyone")
         }
     }
+
+    public val iso_param = 10
+
+    public fun greatEveryone(){
+        Log.e("WELCOME","Hi everyone")
+    }
+
 }
