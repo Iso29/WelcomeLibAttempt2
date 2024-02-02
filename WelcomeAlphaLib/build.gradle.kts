3d03bb18d55a76b2da0df0df0f36e454708e301a
ismayil.islamzade <ismayil.islamzade@azintelecom.az>
2024-02-02 17:12:43 +0400

    new ver 3.7

--- WelcomeAlphaLib/build.gradle.kts
+++ WelcomeAlphaLib/build.gradle.kts
@@ -49,7 +49,7 @@ afterEvaluate {
                 from(components.findByName("release"))
                 groupId = "com.github.iso29"
                 artifactId = "WelcomeAlphaLib"
-                version = "3.5"
+                version = "3.7"
             }
         }
     }
