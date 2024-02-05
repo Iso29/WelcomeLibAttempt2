a541d8ef980bc6bacde9f1af8797a4b1272c9e3c
ismayil.islamzade <ismayil.islamzade@azintelecom.az>
2024-02-05 10:16:07 +0400

    new ver 3.8

--- WelcomeAlphaLib/build.gradle.kts
+++ WelcomeAlphaLib/build.gradle.kts
@@ -12,15 +12,15 @@ android {
         minSdk = 24
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("proguard-rules.pro")
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
         release {
             isMinifyEnabled = true
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                getDefaultProguardFile("proguard-android.txt"),
+                "self-proguard-rules.pro"
             )
         }
     }
@@ -40,6 +40,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    testImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
 }
 afterEvaluate {
     publishing{
