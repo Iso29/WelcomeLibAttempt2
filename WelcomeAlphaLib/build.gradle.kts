a0d76c1370b410ceae5382ae6e7baf86022848df
ismayil.islamzade <ismayil.islamzade@azintelecom.az>
2024-02-06 10:54:57 +0400

    new ver 4.0.5

--- WelcomeAlphaLib/build.gradle.kts
+++ WelcomeAlphaLib/build.gradle.kts
@@ -55,7 +55,7 @@ afterEvaluate {
                 from(components.findByName("release"))
                 groupId = "com.github.iso29"
                 artifactId = "WelcomeAlphaLib"
-                version = "4.0.4"
+                version = "4.0.5"
             }
         }
     }
